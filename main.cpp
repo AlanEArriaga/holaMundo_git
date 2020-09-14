83413372ad0bf4cb0035a2de3e1499b36402fffe
Alan Echeveste <alan.echeveste2963@alumnos.udg.mx>
2020-09-14 17:30:46 -0500

    Capturamos edad y se imprimio edad + 1

--- main.cpp
+++ main.cpp
@@ -4,10 +4,14 @@ using namespace std;
 
 int main(){
     char nombre[100];
+    unsigned int edad;
     cout<<"Hola mundo"<<endl;
     cout<<"Escribe tu nombre: "<<endl;
     cin.getline(nombre,100);
-    cout<<"Hola "<<nombre<<endl;
+    cout<<"Hola buen dia "<<nombre<<endl;
+    cout<<"Cual es tu edad: ";
+    cin>>edad;
+    cout<<endl<<"Vas a cumplir: "<<edad+1<<endl;
 
     return 0;
 }
