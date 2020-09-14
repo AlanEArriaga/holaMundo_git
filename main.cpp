1b640693ba087bc3e1e69559790c4208b4ac9cf2
Alan Echeveste <alan.echeveste2963@alumnos.udg.mx>
2020-09-14 17:21:19 -0500

    Captura nombre completo y saludo usando getline

--- main.cpp
+++ main.cpp
@@ -3,7 +3,11 @@
 using namespace std;
 
 int main(){
+    char nombre[100];
     cout<<"Hola mundo"<<endl;
+    cout<<"Escribe tu nombre: "<<endl;
+    cin.getline(nombre,100);
+    cout<<"Hola "<<nombre<<endl;
 
     return 0;
 }
